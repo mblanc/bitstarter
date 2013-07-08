1741ce24befb00cc080a295b8fea251958d43c17
mblanc <blanc.matthieu@gmail.com>
2013-07-08 15:34:15 +0200

    Modified web.js to read index.html

--- web.js
+++ web.js
@@ -2,11 +2,16 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	fs.readFile('index.html', function (err, data) {
+		if (err) throw err;
+	 	response.send(data.toString());
+	});
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
